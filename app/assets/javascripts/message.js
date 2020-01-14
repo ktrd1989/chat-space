b14c3ab2474fe28990c66942a01814c9722194dc
ktrd1989 <59087649+ktrd1989@users.noreply.github.com>
2020-01-14 21:16:08 +0900

    komitto

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -58,8 +58,8 @@ $('#new_message').on('submit', function(e){
     $('form')[0].reset();
       })
   .fail(function() {
-    alert("メッセージ送信に失敗しました")
-});
+    alert("メッセージ送信に失敗しました");
+})
   .always(function(){
     $(".chat-bottom-message-submit").prop('disabled',false);
     })
