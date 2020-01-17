0de287e62aae69ce6f4143b1511b43a0c41a3dda
ktrd1989 <59087649+ktrd1989@users.noreply.github.com>
2020-01-17 16:41:33 +0900

    test

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the Rails application.
 require_relative 'application'
 
-config.consider_all_requests_local = true
+# config.consider_all_requests_local = true
 
 # Initialize the Rails application.
 Rails.application.initialize!
